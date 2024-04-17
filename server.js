3e470cfe1dea6c189827c2953ceaa0ef4732247a
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-04-17 00:14:58 +0000

    trying to fix deployment error

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(express.json());
 // Routes
 // GET /notes - Return the notes.html file
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
 // GET * - Return the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // GET /api/notes - Read the db.json file and return all saved notes as JSON
